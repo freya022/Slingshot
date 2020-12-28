032e8b42b52955a6be4fd3746b2c65dceefb4445
freya022 <41875020+freya022@users.noreply.github.com>
2020-12-28 23:46:45 +0100

    Remove ResourceHacker and replace by passing the resource file at link-time

--- SVMFX/Compile.bat
+++ SVMFX/Compile.bat
@@ -1,22 +1,7 @@
 @echo off
 
-set sourceFolder=../target/client/x86_64-windows
-set executableName=Slingshot.exe
-set inputExe="%sourceFolder%/%executableName%"
-set iconPath=..\Icon.ico
-
 if not exist ..\pom.xml (
 	echo "Not a valid Maven project"
-	pause
-)
-
-if not exist %sourceFolder% (
-	echo "Invalid source folder path : %sourceFolder%"
-	pause
-)
-
-if not exist %iconPath% (
-	echo "Invalid icon path : %iconPath%"
 	pause
 )
 
@@ -25,14 +10,17 @@ if %errorlevel% == 9009 (
 	call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
 
-set /p continue=Are you sure you want to compile %executableName% ? (y/n) : 
+set /p continue=Are you sure you want to compile and link ? (y/n) :
 
 if "%continue%" == "y" (
 	cd ..
-	mvn client:build
+	mvn client:compile
 	cd SVMFX
 
-	start /WAIT ResourceHacker.exe -open "Test.rc" -save "Test.res" -action compile
-	start /WAIT ResourceHacker.exe -open %inputExe% -save %inputExe% -action addoverwrite -res "Test.res"
-	start /WAIT ResourceHacker.exe -open %inputExe% -save %inputExe% -action addoverwrite -res "%iconPath%" -mask ICONGROUP,MAINICON
+	start /WAIT rc.exe Resources.rc
+	start /WAIT cvtres.exe Resources.res
+	
+	cd ..
+	mvn client:link
+	cd SVMFX
 )
