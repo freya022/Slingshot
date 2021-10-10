b23c619cc2fa638350645fb5660a897b1af2c225
freya022 <41875020+freya022@users.noreply.github.com>
2021-10-10 12:17:30 +0200

    Remove unnecessary non-capturing group, allow uppercase in emails

--- src/main/java/com/freya02/slingshot/auth/AuthController.java
+++ src/main/java/com/freya02/slingshot/auth/AuthController.java
@@ -22,7 +22,7 @@ import java.util.regex.Pattern;
 public class AuthController extends LazyWindow {
 	private static final String INVALID_TEXT_ID = "invalidText";
 	private static final Pattern usernamePattern = Pattern.compile("[\\w]{3,16}");
-	private static final Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])");
+	private static final Pattern emailPattern = Pattern.compile("(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])");
 	private static final Matcher usernameMatcher = usernamePattern.matcher("");
 	private static final Matcher emailMatcher = emailPattern.matcher("");
 
@@ -94,7 +94,8 @@ public class AuthController extends LazyWindow {
 			try {
 				final Credentials credentials = Credentials.getInstance();
 				if (emailMatcher.reset(identifierField.getText()).matches()) {
-					final String[] info = authenticate0(identifierField.getText(), passwordField.getText(), UUID.randomUUID().toString());
+					final String emailAddress = identifierField.getText().toLowerCase();
+					final String[] info = authenticate0(emailAddress, passwordField.getText(), UUID.randomUUID().toString());
 
 					credentials.setUsername(info[0]);
 					credentials.setUuid(info[1]);
